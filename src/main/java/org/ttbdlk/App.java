d7c7facae74d2421161d3658ead8ba55ef2ad700
T0ughy <beresdavid0908@gmail.com>
2020-04-18 12:00:08 +0200

    Feltöltöttem a két adatbázist és létrehoztam egy új osztályt, fel is vannak kommentezve

--- src/main/java/org/ttbdlk/App.java
+++ src/main/java/org/ttbdlk/App.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Date;
 
 /**
  * JavaFX App
@@ -33,13 +34,28 @@ public class App extends Application {
 
     public static void main(String[] args) {
         DbConnect connect = new DbConnect();
-        connect.getData();
-        Team csapat = new Team(3, "Vikings", "25");
-        connect.pushData(csapat);
-        connect.getData();
-        connect.deleteData(1);
-        connect.getData();
+        /*
+        csinálok egy Team tömböt, mert ebben a hajnali órában így tűnik a legeffektívebbnek, hogy utána egy for ciklussal feltöltsem a teams táblát
+        Team[] pushingTeamsArray=new Team[32];
+
+        belerakom egyesével ugye mind a 32 csapatot
+        pushingTeamsArray[0]=new Team("Arizona Cardinals", "NFC West", "Kliff Timothy Kingsbury", "Michael Bidwill");
+
+        a Teams tábla feltöltése
+        connect.pushDataToTeams(pushingTeamsArray[i]);
+
+        hibajavítás
+        connect.updateData(new Team("Green Bay Packers", "NFC North", "Mark Murphy"));*/
+
+        //Player tömb a feltöltéshez, csak 10 darab, mert elég még
+        //Player[] pushingPlayersArray=new Player[10];
+
+        //egyesével a 10 játékost belerakom a tömbbe
+        //pushingPlayersArray[0]=new Player(31, "Tyler Biadasz", "Wisconsin", "C", Date.valueOf("1997-05-17"),191, 143, "San Fransisco 49ers");
+
+        //a Players tábla feltöltése
+        //connect.pushDataToPlayers(pushingPlayersArray[i]);
+
         launch();
     }
-
 }
